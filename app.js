fbfccff0256b75b9ecbddb462db6a771c2e9bc16
SuperCoder <propscode254@gmail.com>
2023-12-27 09:12:49 +0100

    change json

---
+++ app.js
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+// import { router as moviesRouter } from "./movies/router.js";
+// import { router as favoritesRouter } from "./favorites/router.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()); //body parser -> header 'Content-Type': 'application/json'
+
+// app.use("/uploads", express.static("./uploads"));
+// app.use("/api", moviesRouter);
+// app.use("/api", favoritesRouter);
+
+// app.listen(process.env.PORT, () =>
+//   console.log(console.log("Port is:http://localhost:" + process.env.PORT))
+// );
